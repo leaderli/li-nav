df35ab3d36d7f786ea85ce4c65e9d530704c072b
leaderli <429243408@qq.com>
2021-12-08 23:51:11 +0800

    gitignore

--- src/api/bookmarks_api.js
+++
@@ -1,5 +0,0 @@
-import axios from '@/util/request';
-export const bookmarks_api = async function () {
-    return await axios("/bookmarks");
-};
-//# sourceMappingURL=bookmarks_api.js.map
--- src/type/bookmark_type.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=bookmark_type.js.map
