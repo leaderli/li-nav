191715b0e9d646378d1ffff205ce0720a212d836
li <li@192.168.159.128>
2021-12-15 04:03:07 +0800

    v10

--- start_json_server.sh
+++ start_json_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ${HOME}/.bashrc
 
-nohup json-server --watch /home/li/app/json_server/db.json --host 0.0.0.0 --port 10004  2>&1 &
+nohup json-server --watch /home/li/app/li-nav/db.json --host 0.0.0.0 --port 10004  2>&1 &
 
-echo "$!" > /home/li/app/json_server/json_server.pid
+echo "$!" > /home/li/app/li-nav/json_server.pid
 
