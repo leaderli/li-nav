e36948f576a1d5457d6e5dd2cf1c372c161b4980
leaderli <429243408@qq.com>
2021-12-18 21:03:08 +0800

    123

--- src/api/bookmarks_api.ts
+++ src/api/bookmarks_api.ts
@@ -1,11 +1,18 @@
-import  axios from '@/util/request'
+import axios from '@/util/request'
 import {bookmark_type} from "@/type/bookmark_type";
 
 
+export const bookmarks_api = async function () {
 
-export  const  bookmarks_api = async function (){
-
-    return await axios<void,Array<bookmark_type>>("/api/bookmarks")
+    return await axios<void, Array<bookmark_type>>("/api/bookmarks")
 }
 
+
+export const update_bookmarks_api = async function (bookmarks: Array<bookmark_type>) {
+
+    return await axios('/api/bookmarks', 'PUT', {
+        dataType:'json',
+        data: bookmarks
+    })
+}
 
